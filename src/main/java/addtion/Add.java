95e65a1c0c333282b887773e637be59189cc83b8
namilakondab <namilakondab@HDDLBGKIOSK10.Virtusa.com>
2019-06-17 17:18:46 +0530

    commited

---
+++ src/main/java/addtion/Add.java
@@ -0,0 +1,15 @@
+package addtion;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int first = 10;
+	        int second = 20;
+
+	        int sum = first + second;
+
+	        System.out.println("The sum is: " + sum);
+	}
+
+}
